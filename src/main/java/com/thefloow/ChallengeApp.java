9f45dad21bddb832e2c289cfa8c4fd9abf96a8e2
SammyMarie <sammymarie@localhost.localdomain>
2017-07-04 12:28:09 +0100

    Initial Project setup

---
+++ src/main/java/com/thefloow/ChallengeApp.java
@@ -0,0 +1,15 @@
+package com.thefloow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by sammymarie on 04/07/17.
+ */
+@SpringBootApplication
+public class ChallengeApp {
+
+    public static void main(String... args){
+        SpringApplication.run(ChallengeApp.class, args);
+    }
+}
