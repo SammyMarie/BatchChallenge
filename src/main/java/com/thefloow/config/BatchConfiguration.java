dfe12153081502278d7b9227ae12241b5b14781d
sammymarie <sml.ifere@gmail.com>
2017-07-04 16:59:39 +0100

    Refactors to show alternative job state transition

--- src/main/java/com/thefloow/config/BatchConfiguration.java
+++ src/main/java/com/thefloow/config/BatchConfiguration.java
@@ -2,9 +2,12 @@ package com.thefloow.config;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -54,9 +57,8 @@ public class BatchConfiguration {
     public Job transitionJob(){
         return jobBuilderFactory.get("transitionJob")
                 .start(stepOne())
-                .on("COMPLETED").to(stepTwo())
-                .from(stepTwo()).on("COMPLETED").to(stepThree())
-                .from(stepThree()).end()
+                .next(stepTwo())
+                .next(stepThree())
                 .build();
     }
 }
