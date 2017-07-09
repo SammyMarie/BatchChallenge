b642d9992a7936258b00bc4a3cf8233d5eb8efe7
Samuel Ifere <sml.ifere@gmail.com>
2017-07-09 12:08:37 +0100

    Adds monogodb config for writing processed data

--- src/main/java/com/thefloow/config/BatchConfiguration.java
+++ src/main/java/com/thefloow/config/BatchConfiguration.java
@@ -1,24 +1,22 @@
 package com.thefloow.config;
 
-import com.thefloow.model.Page;
-import lombok.Builder;
+import com.mongodb.Mongo;
+import com.thefloow.model.Mediawiki;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.job.builder.FlowBuilder;
-import org.springframework.batch.core.job.flow.Flow;
-import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.data.MongoItemWriter;
 import org.springframework.batch.item.file.MultiResourceItemReader;
 import org.springframework.batch.item.xml.StaxEventItemReader;
-import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.data.mongodb.core.MongoClientFactoryBean;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.oxm.xstream.XStreamMarshaller;
 
 import java.util.HashMap;
@@ -34,6 +32,12 @@ public class BatchConfiguration {
     @Value("${data.input}")
     private Resource[] dataInput;
 
+    @Value("${spring.data.mongodb.database}")
+    private String mongoDb;
+
+    @Autowired
+    private Mongo mongo;
+
     @Autowired
     private JobBuilderFactory jobBuilderFactory;
 
@@ -41,8 +45,8 @@ public class BatchConfiguration {
     private StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public MultiResourceItemReader<Page> multiResourceItemReader(){
-        MultiResourceItemReader<Page> reader = new MultiResourceItemReader<>();
+    public MultiResourceItemReader<Mediawiki> multiResourceItemReader(){
+        MultiResourceItemReader<Mediawiki> reader = new MultiResourceItemReader<>();
 
         reader.setDelegate(pageItemReader());
         reader.setResources(dataInput);
@@ -51,46 +55,59 @@ public class BatchConfiguration {
     }
 
     @Bean
-    public StaxEventItemReader<Page> pageItemReader(){
+    public StaxEventItemReader<Mediawiki> pageItemReader(){
 
         XStreamMarshaller unmarshaller = new XStreamMarshaller();
 
         Map<String, Class> aliases = new HashMap<>();
-        aliases.put("page", Page.class);
+        aliases.put("mediawiki", Mediawiki.class);
 
         unmarshaller.setAliases(aliases);
 
         StaxEventItemReader reader = new StaxEventItemReader();
-        reader.setFragmentRootElementName("page");
+        reader.setFragmentRootElementName("mediawiki");
         reader.setUnmarshaller(unmarshaller);
 
         return reader;
     }
 
     @Bean
-    public ItemWriter<Page> pageItemWriter() {
-        return items -> {
-            for(Page item : items){
-                System.out.println(item.toString());
-            }
-        };
+    public MongoItemWriter<Mediawiki> pageItemWriter() throws Exception {
+        MongoItemWriter<Mediawiki> itemWriter = new MongoItemWriter<>();
+
+        itemWriter.setCollection("customers");
+        itemWriter.setTemplate(mongoTemplate(mongo));
+        itemWriter.afterPropertiesSet();
+
+        return itemWriter;
     }
 
     @Bean
-    public Step stepOne(){
+    public Step stepOne() throws Exception{
 
         return stepBuilderFactory.get("stepOne")
-                .<Page, Page>chunk(100000)
+                .<Mediawiki, Mediawiki>chunk(100000)
                 .reader(pageItemReader())
                 .writer(pageItemWriter())
                 .build();
     }
 
     @Bean
-    public Job transitionJob(){
+    public Job transitionJob() throws Exception{
 
         return jobBuilderFactory.get("transitionJob")
                                 .start(stepOne())
                                 .build();
+    }
+
+    @Bean
+    public MongoClientFactoryBean mongoDbFactory(){
+        MongoClientFactoryBean mongoClient = new MongoClientFactoryBean();
+        return mongoClient;
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate(Mongo mongo){
+        return new MongoTemplate(mongo, mongoDb);
     }
 }
