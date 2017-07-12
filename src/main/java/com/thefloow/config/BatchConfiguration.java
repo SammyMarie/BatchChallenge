4bae202bf3939892814989c88283f5303d789aba
Samuel Ifere <sml.ifere@gmail.com>
2017-07-12 22:44:39 +0100

    Updates max thread pool size to match grid size

--- src/main/java/com/thefloow/config/BatchConfiguration.java
+++ src/main/java/com/thefloow/config/BatchConfiguration.java
@@ -95,7 +95,7 @@ public class BatchConfiguration {
     public ThreadPoolTaskExecutor taskExecutor() {
         ThreadPoolTaskExecutor pool = new ThreadPoolTaskExecutor();
         pool.setCorePoolSize(5);
-        pool.setMaxPoolSize(10);
+        pool.setMaxPoolSize(8);
         pool.setWaitForTasksToCompleteOnShutdown(true);
         return pool;
     }
